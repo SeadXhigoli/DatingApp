fdeb444b04127dcd65ce99042d843c2a58ff1d90
Sead <Lenovo@LAPTOP-H1U9VIJ6.com>
2020-09-08 23:11:24 +0200

    initial Commit

---
+++ DatingAPI/DatingAPI/Migrations/20200906105344_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Values",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Values", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Values");
+        }
+    }
+}
---
+++ DatingAPI/DatingAPI/Models/DatingAppContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingAPI.Models
+{
+    public class DatingAppContext:DbContext
+    {
+        public DatingAppContext(DbContextOptions<DatingAppContext> options):base(options)
+        {
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
